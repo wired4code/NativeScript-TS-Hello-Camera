a34adee09958d13d50f49f9e5c83f4ce65198f00
Sonny Sheth <sonny.sheth@gmail.com>
2017-04-22 08:50:14 -0300

    update code

--- app/main-page.ts
+++ app/main-page.ts
@@ -3,39 +3,39 @@ In NativeScript, a file with the same name as an XML file is known as
 a code-behind file. The code-behind is a great place to place your view
 logic, and to set up your page’s data binding.
 */
-
+import { EventData, Observable, fromObject } from "data/observable";
 import { Page } from 'ui/page';
-import * as camera from "nativescript-camera";
+import { View } from 'ui/core/view';
+import { takePicture } from "nativescript-camera";
 import { Image } from "ui/image";
+import * as appModule from "application";
+import * as imageSourceModule from "image-source";
 
-const options = { width: 150, height: 150, keepAspectRatio: true, saveToGallery: true };
-let myImage;
+const options = { width: 300, height: 300, keepAspectRatio: false, saveToGallery: true };
 let cameraPermissionReceived = false;
 
-export function pageLoaded (args) {
+export function navigatingTo(args: EventData) {
+    let page = <Page>args.object;
+    let picturePath = null;
 
-    const page = args.object;
-    myImage = page.getViewById("myImage");
-    myImage.src = "https://placehold.it/150x150";
-    page.bindingContext = {};
+    page.bindingContext = fromObject({cameraImage: picturePath, saveToGallery: true});
 }
 
-export function onTap () {
 
-    if (!cameraPermissionReceived) {
-        openCamera();
-        cameraPermissionReceived = true;
-        return;
-    }
+export function onTakePictureTap (args: EventData) {
 
-    return camera.isAvailable() ? openCamera() : alert("No device camera");
-}
+    let page = <Page>(<View>args.object).page;
+    let saveToGallery = page.bindingContext.get("saveToGallery");
+    takePicture({width: 180, height: 180, keepAspectRatio: false, saveToGallery: saveToGallery}).
+        then((imageAsset) => {
+            let source = new imageSourceModule.ImageSource();
+            source.fromAsset(imageAsset).then((source) => {
+                console.log(`Size: ${source.width}x${source.height}`);
+            });
+            page.bindingContext.set("cameraImage", imageAsset);
+        },
+        (err) => {
+            console.log("Error -> " + err.message);
+        });
 
-export function openCamera () {
-    camera.takePicture(options)
-        .then((imageAsset) => {
-            console.log("Result is an image asset instance");
-            myImage = new Image();
-            myImage.src = imageAsset;
-        }).catch( err => console.log("Error -> " + err.message))
 }
